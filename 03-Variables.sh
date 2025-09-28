d7abd9a28b0d432416575b7a5af2b6412991a16d
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-09-28 17:33:36 +0530

    variables code

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Rakesh
+PERSON2=suresh
+
+echo "$PERSON1::Hello $PERSON2"
+echo "$PERSON1:: Helloe $PERSON1"
+echo "$PERSON2::how are you $PERSON1"
+echo "$PERSON2::i am fine $PERSON2 how are you"
+echo "$PERSON2::i am good $PERSON1" 
