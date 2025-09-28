7090ca9c84f0dc7eca6b2853b720cbfe57dbfe96
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-09-28 17:25:18 +0530

    conservations 2

---
+++ 02-conseravtion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is commented out code
+echo "Hello World!"
+echo "Rakesh :My name i rakesh"
+echo "RameshI am learning DevOps"
+echo "I am learning Jenkins"
+echo "I am learning Docker"
