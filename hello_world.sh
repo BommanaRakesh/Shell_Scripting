b533c725b9c3b2111ea9eb32b05e23d82b86c499
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-09-28 17:21:13 +0530

    added fw lines

--- hello_world.sh
+++ hello_world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # This is commented out code
 echo "Hello World!"
+echo "My name i rakesh"
+echo "I am learning DevOps"
+echo "I am learning Jenkins"
+echo "I am learning Docker"
+echo "I am learning Kubernetes"        
