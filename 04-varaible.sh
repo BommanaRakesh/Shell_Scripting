a177cf14126f02a3f38b8b51a0feee8b615c2a74
bommanarakesh <bommanarakesh1998@gmail.com>
2025-10-11 14:35:19 +0530

    commmitted

---
+++ 04-varaible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hello $PERSON2"
+echo "$PERSON1:: Helloe $PERSON1"
+echo "$PERSON2::how are you $PERSON1"
+echo "$PERSON2::i am fine $PERSON2 how are you"
+echo "$PERSON2::i am good $PERSON1" 
