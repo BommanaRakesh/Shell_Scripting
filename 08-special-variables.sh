7d64f34da3ec206a33d271fde9767cd3e4f72cda
bommanarakesh <bommanarakesh1998@gmail.com>
2025-10-12 10:21:11 +0530

    your commit message

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,6 @@
 echo "All vaibles assd to th script are:$@"
 echo "Allvariables passed to the script are:$*"
 echo "script name:$0"
+echo "current directory is:$PWD"
+echo "current user is:$USER"
+echo "home directory is:$HOME"
