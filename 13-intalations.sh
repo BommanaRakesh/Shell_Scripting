5c28dece1be7e645adad6441eaee5e359bfbf8e6
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-11-09 13:26:55 +0530

    commit

--- 13-intalations.sh
+++ 13-intalations.sh
@@ -1,14 +1,12 @@
 #!/bash/bin
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ];
-then
+if [ $USERID -ne 0 ];then
   echo "You must be root to run this script."
 exit 1
 fi
 dnf install mysql -y
-if[ $? -ne 0 ];
-then
+if[ $? -ne 0 ];then
   echo "MySQL installation failed."
 exit 1
 else
