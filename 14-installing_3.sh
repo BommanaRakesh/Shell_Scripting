7bacff369125b79f973f885961429d041af51579
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-11-09 16:29:49 +0530

    some message

--- 14-installing_3.sh
+++ 14-installing_3.sh
@@ -1,37 +1,24 @@
-# #!/bin/bash
-
-# USERID=$(id -u)
-# if [ $USERID -ne 0 ];
-# then
-#   echo "You must be root to run this script."
-#     exit 1
-# fi
-# dnf install mysql -y
-# if[ $? -ne 0 ];
-# then
-#   echo "ERRor:MySQL installation failed."
-#   exit 1
-#   else
-# echo "MySQL installed successfully."
-# fi
-
-!/bin/bash
-
+#!/bin/bash
 USERID=$(id -u)
-
-# Check if the user is root
-if [ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ];then
   echo "You must be root to run this script."
   exit 1
 fi
+validate_installation() {
+  if [ $1 -ne 0 ];then
+    echo " installation $2 is failure"
+    exit 1
+  else
+    echo "$2 installed successfully."
+  fi
+} 
 
-# Install MySQL
 dnf install mysql -y
+validate $? "MySQL" 
 
-# Check if the installation was successful
-if [ $? -ne 0 ]; then
-  echo "ERROR: MySQL installation failed."
-  exit 1
-else
-  echo "MySQL installed successfully."
-fi
+dnf install nginx -y
+validate $? "Nginx" 
+
+dnf install mangodb-mangosh -y
+validate $? "mangosh"
+
