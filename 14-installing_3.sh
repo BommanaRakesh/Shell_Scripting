ea1e90648168c64d87c770374e1aac92a9f6a84f
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-11-09 13:17:27 +0530

    commit

--- 14-installing_3.sh
+++ 14-installing_3.sh
@@ -1,16 +1,37 @@
-#!/bin/bash
+# #!/bin/bash
+
+# USERID=$(id -u)
+# if [ $USERID -ne 0 ];
+# then
+#   echo "You must be root to run this script."
+#     exit 1
+# fi
+# dnf install mysql -y
+# if[ $? -ne 0 ];
+# then
+#   echo "ERRor:MySQL installation failed."
+#   exit 1
+#   else
+# echo "MySQL installed successfully."
+# fi
+
+!/bin/bash
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ];
-then
+
+# Check if the user is root
+if [ $USERID -ne 0 ]; then
   echo "You must be root to run this script."
-    exit 1
-fi
-dnf install mysql -y
-if[ $? -ne 0 ];
-then
-  echo "ERRor:MySQL installation failed."
   exit 1
-  else
-echo "MySQL installed successfully."
+fi
+
+# Install MySQL
+dnf install mysql -y
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+  echo "ERROR: MySQL installation failed."
+  exit 1
+else
+  echo "MySQL installed successfully."
 fi
