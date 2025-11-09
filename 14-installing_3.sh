5af91c34d8b25ebe8aa71b8334ee9cb53c0ee053
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-11-09 16:32:52 +0530

    some message

--- 14-installing_3.sh
+++ 14-installing_3.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+
 USERID=$(id -u)
-if [ $USERID -ne 0 ];then
+if [ "$USERID" -ne 0 ]; then
   echo "You must be root to run this script."
   exit 1
 fi
-validate_installation() {
-  if [ $1 -ne 0 ];then
-    echo " installation $2 is failure"
+
+# usage: validate <exit_code> <name>
+validate() {
+  if [ "$1" -ne 0 ]; then
+    echo "ERROR: $2 installation failed."
     exit 1
   else
     echo "$2 installed successfully."
   fi
-} 
+}
 
-dnf install mysql -y
-validate $? "MySQL" 
+# MySQL server (use 'mariadb-server' if 'mysql-server' isn't available)
+dnf -y install mysql-server
+validate $? "MySQL server"
 
-dnf install nginx -y
-validate $? "Nginx" 
+# Nginx
+dnf -y install nginx
+validate $? "Nginx"
 
-dnf install python3 -y
-validate $? "python3"
-
+# Python 3
+dnf -y install python3
+validate $? "Python 3"
