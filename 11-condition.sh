72fce12c73b2a6c3095c59fbe9414c475feeac89
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-11-09 12:41:22 +0530

    commit

---
+++ 11-condition.sh
@@ -0,0 +1,12 @@
+#!/bash/bin
+
+echo : "Enter a number: "
+read Number 
+
+
+if [ $((Number % 2)) -eq 0 ] 
+then
+  echo "Number is $Number and it is even"
+  else 
+  echo "Number is $Number and it is even"
+fi
