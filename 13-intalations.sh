8cb3f5124016d62caff04417b54c58792955b063
BommanaRakesh <bommanarakesh1998@gmail.com>
2025-11-09 12:58:50 +0530

    commit

---
+++ 13-intalations.sh
@@ -0,0 +1,14 @@
+#!/bash/bin
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ];
+then
+  echo "You must be root to run this script."
+fi
+dnf install hmysql -y
+if[ $? -ne 0 ];
+then
+  echo "MySQL installation failed."
+  exit 1
+fi
+echo "MySQL installed successfully."
